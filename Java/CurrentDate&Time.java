f4c2c0af6a1b8a1f5668fa0de89d835303daa462
Tanmayee-Damodara <89707087+Tanmayee-Damodara@users.noreply.github.com>
2022-12-13 13:01:34 +0530

    Create CurrentDate&Time.java

---
+++ Java/CurrentDate&Time.java
@@ -0,0 +1,17 @@
+import java.text.*;
+import java.util.*;
+public class GFG {
+    public static void main(String args[])
+    {
+        SimpleDateFormat formatDate = new SimpleDateFormat("dd/MM/yyyy  HH:mm:ss z");
+        
+      Date date = new Date();
+        // initialize "Date" class
+ 
+        formatDate.setTimeZone(TimeZone.getTimeZone("IST"));
+        // converting to IST or format the Date as IST
+ 
+        System.out.println(formatDate.format(date));
+        // print formatted date and time
+    }
+}
