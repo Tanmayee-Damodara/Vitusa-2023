5f164177c7e3896f219d95720bd9eaa6d5add24f
Tanmayee-Damodara <89707087+Tanmayee-Damodara@users.noreply.github.com>
2022-12-13 13:31:50 +0530

    Create EmployeeDetails.sql

---
+++ SQL-task/EmployeeDetails.sql
@@ -0,0 +1,3 @@
+INSERT INTO EmployeeDetails values(121, 'John Snow', 321, '31-JAN-2014', 'Toronto') 
+INSERT INTO EmployeeDetails values(321, 'Walter White', 986, '30-JAN-2015', 'California') 
+INSERT INTO EmployeeDetails values(421, 'Kuldeep Rana', 876, '27-NOV-2016', 'New Delhi') 
